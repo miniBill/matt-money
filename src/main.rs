4f70041de2a445cb97f0e575c66c8d078603917c
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-07-09 20:21:33 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,57 +3,68 @@ use rayon::prelude::*;
 type Coin = u16;
 
 fn main() {
-    let max_change = 500;
-    let max_coin_denomination = 200;
-    let max_coin_count = 9;
+    for n in 5..=20 {
+        let max_change = n;
+        let max_coin_denomination = n;
+        let max_coin_count = n;
 
-    for coin_count in 2..=max_coin_count {
-        use std::time::Instant;
-        let start = Instant::now();
+        for coin_count in 2..=max_coin_count {
+        // for coin_count in 5..=5 {
+            use std::time::Instant;
+            let start = Instant::now();
 
-        if let Some((solution, total)) = (IteratorState {
-            max_coin_denomination,
-            state: (0..coin_count).collect::<Vec<_>>(),
-        })
-        .par_bridge()
-        .map(|coins| {
-            let count = count_coins(max_change, &coins);
-            (coins, count)
-        })
-        .fold(
-            || (vec![], (max_change * max_change) as usize),
-            |(mut acc, best), (coins, total)| match total.cmp(&best) {
-                std::cmp::Ordering::Less => (vec![coins], total),
-                std::cmp::Ordering::Equal => {
-                    acc.push(coins);
-                    (acc, best)
+            if let Some((mut solution, total)) = (IteratorState {
+                max_coin_denomination,
+                state: (0..coin_count).collect::<Vec<_>>(),
+            })
+            .par_bridge()
+            .map(|coins| {
+                let count = count_coins(max_change, &coins);
+                (coins, count)
+            })
+            .fold(
+                || (vec![], (max_change * max_change) as usize),
+                |(mut acc, best), (coins, total)| match total.cmp(&best) {
+                    std::cmp::Ordering::Less => (vec![coins], total),
+                    std::cmp::Ordering::Equal => {
+                        acc.push(coins);
+                        (acc, best)
+                    }
+                    std::cmp::Ordering::Greater => (acc, best),
+                },
+            )
+            .reduce_with(|(mut left, left_best), (mut right, right_best)| {
+                match left_best.cmp(&right_best) {
+                    std::cmp::Ordering::Less => (left, left_best),
+                    std::cmp::Ordering::Equal => {
+                        left.append(&mut right);
+                        (left, left_best)
+                    }
+                    std::cmp::Ordering::Greater => (right, right_best),
                 }
-                std::cmp::Ordering::Greater => (acc, best),
-            },
-        )
-        .reduce_with(|(mut left, left_best), (mut right, right_best)| {
-            match left_best.cmp(&right_best) {
-                std::cmp::Ordering::Less => (left, left_best),
-                std::cmp::Ordering::Equal => {
-                    left.append(&mut right);
-                    (left, left_best)
-                }
-                std::cmp::Ordering::Greater => (right, right_best),
-            }
-        })
-        // .min_by_key(|(_, total)| *total)
-        {
-            println!(
-                "The best result for {coin_count} coins is an average of {} coins, found in {:.2?} in {} possible solutions (printing at most 5)",
-                (total as f64) / (max_change as f64),
-                start.elapsed(),
-                solution.len()
-            );
+            })
+            // .min_by_key(|(_, total)| *total)
+            {
+                let average: f64 = (total as f64) / (max_change as f64);
+                println!(
+                    "max_change {max_change:3} coin_count {coin_count} solutions count {:3} time {:3.2?} average {average}",
+                    solution.len(),
+                    start.elapsed()
+                );
 
-            for coins in solution.into_iter().take(5) {
-                println!("  {:?}", coins);
+                solution.sort();
+
+                for coins in solution.into_iter().take(5) {
+                    for (i, coin) in coins.iter().enumerate() {
+                        if i == 0 {
+                            print!("  ");
+                        }
+                        print!("{coin:3}");
+                    }
+                    println!();
+                }
+                // println!("  {:?}", solution);
             }
-            // println!("  {:?}", solution);
         }
     }
 }
